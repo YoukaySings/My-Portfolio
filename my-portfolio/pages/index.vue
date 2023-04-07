302c4423f66f483c2301e2e2da74f050c83a6c95
YoukaySings <iteslimidris09@gmail.com>
2023-04-07 13:56:07 +0100

    page-six

--- my-portfolio/pages/index.vue
+++ my-portfolio/pages/index.vue
@@ -4,7 +4,7 @@
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200&display=swap');
-  p, h2, i, button {
+  p, h2, i, button, ul {
     font-family: 'Montserrat', sans-serif;
   }
 </style>
